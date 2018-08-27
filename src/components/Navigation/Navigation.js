62de8b4ee463241e246b8cbeaad233a2e2254bc3
Thomas Hanna <thomasjhanna@gmail.com>
2018-08-27 14:51:32 +0100

    Initial commit

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './Navigation.css';
+
+
+const Navigation = ({ onRouteChange }) => {
+  return (
+    <nav>
+      <p onClick={() => onRouteChange('signin')} className="f3 link dim black underline pa3 pointer">Sign Out</p>  
+    </nav>
+  );
+}
+
+export default Navigation;
